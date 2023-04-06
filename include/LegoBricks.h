6f89406a2c07319af52f110bdc693a1520d4870d
Léo Parent <ired4321@gmail.com>
2023-04-06 15:48:31 +0200

    allLegos

--- include/LegoBricks.h
+++ include/LegoBricks.h
@@ -14,75 +14,57 @@
 
 void classicBar(float thickness, int rings, int ns, int nl);
 
-/*
-	thin lego technic L brick
-	size : 3x3
-*/
-// void liftarmThin1x2AxleHoles_4163533();
-// void technicPanelFairing22_4211807();
-// void technicAxleAndPinConnector1_6332573();
-// void technicAxleAndPinConnectorPerpendicular3LWith2PinHoles_6330960();
-// void wheel18x14mmWithPinHole_6359828();
-// void technicDrivingRingConnector_6276052();
-// void technicPinLongWithoutFrictionRidgesLengthwise_6321305();
-// void technicAxle4WithCenterStop_4666999();
-// void technicBrick1x4WithHoles_4234365();
-// void plate4x8_4509897();
-// void technicGear12ToothDoubleBevel_4177431();
-// void technicLiftarm1x2Thick_4177444();
-// void technicLiftarm1x3Thin_6331723();
-// void TechnicLiftarm1x4Thin_6327548();
-// void technicLiftarm1x5Thick_4249021();
-// void technicLiftarm1x6Thin_6345239();
-// void technicLiftarm3x3LShapeThin_6271810();
-// void technicLiftarm1x2ThickWithPinHoleAndAxleHole_6265135();
-// void technicAxlePin3LWithFrictionRidgesLengthwiseAnd2LAxle_6089119();
-// void technicAxleAndPinConnectorPerpendicular3LWithCenterPinHole_6276951();
-// void technicAxle4_370526();
-// void technicLiftarm2x4LShapeThick_6271825();
-// void tire304x14OffsetTreadBandAroundCenterOfTread_4619323();
-// void technicLiftarm3x5LShapeWithQuarterEllipseThin_6327162();
-// void technicPinWithFrictionRidgesLengthwiseAndPinHole_6282140();
-// void technicLiftarm3x3TShapeThick_4552347();
-// void technicLiftarm1x5Thick_4142135();
-// void technicLiftarm1x7Thick_4495935();
-// void technicAxle8_370726();
-// void technicAxle12_370826();
 
-
-// same but in alphabetical order
-
+void gear8ToothType2_6012451();
+void liftarmThin1x2AxleHoles_4163533();
 void plate4x8_4509897();
-void technicAxle12_370826();
+void technicAxle2Notched_4142865();
+void technicAxle3_4211815();
 void technicAxle4_370526();
 void technicAxle4WithCenterStop_4666999();
+void technicAxle5WithStop_6159763();
 void technicAxle8_370726();
+void technicAxle12_370826();
 void technicAxleAndPinConnector1_6332573();
 void technicAxleAndPinConnectorPerpendicular3LWith2PinHoles_6330960();
 void technicAxleAndPinConnectorPerpendicular3LWithCenterPinHole_6276951();
+void technicAxleAndPinConnectorPerpendicular_6261373();
+void technicAxleConnectorSmoothWithXHoleOrientation_4512360();
 void technicAxlePin3LWithFrictionRidgesLengthwiseAnd2LAxle_6089119();
-void technicBrick1x4WithHoles_4234365();
+void technicAxlePin3LWithFrictionRidgesLengthwise_6209519();
+void technicAxlePinWithFrictionRidgesLengthwise_4206482();
+void technicBush_6275844();
+void technicBush1By2Smooth_6271165();
 void technicDrivingRingConnector_6276052();
 void technicGear12ToothDoubleBevel_4177431();
+void technicGearWormScrew_6185471();
 void technicLiftarm1x2Thick_4177444();
 void technicLiftarm1x2ThickWithPinHoleAndAxleHole_6265135();
+void technicLiftarm1x2ThickWithPinHoleAndAxleHole_6344864();
 void technicLiftarm1x3Thin_6331723();
 void technicLiftarm1x4Thin_6327548();
+void technicLiftarm1x4Thin_6364749();
 void technicLiftarm1x5Thick_4142135();
 void technicLiftarm1x5Thick_4249021();
 void technicLiftarm1x6Thin_6345239();
 void technicLiftarm1x7Thick_4495935();
+void technicLiftarm1x13Thick_6261643();
+void technicLiftarm1x115DoubleBentThick_6271156();
 void technicLiftarm2x4LShapeThick_6271825();
 void technicLiftarm3x3LShapeThin_6271810();
 void technicLiftarm3x3TShapeThick_4552347();
+void technicLiftarm3x5LShapeThick_6173003();
 void technicLiftarm3x5LShapeWithQuarterEllipseThin_6327162();
+void technicLiftarm3X5PerpendicularHShapeThick_6055519();
+void technicPinConnectorPerpendicular3LWith4Pins_6282158();
+void technicPinLongWithFrictionRidgesLengthwise_6299413();
 void technicPinLongWithoutFrictionRidgesLengthwise_6321305();
+void technicPinWithoutFrictionRidgesLengthwise_4211807();
 void technicPinWithFrictionRidgesLengthwiseAndPinHole_6282140();
-void technicPanelFairing22_4211807();
-void technicWheel18x14mmWithPinHole_6359828();
+void technicPinWithFrictionRidgesLengthwise_4206482();
+void technicTread_6271164();
+void technicTreadWithCenterPinHole_6271163();
 void tire304x14OffsetTreadBandAroundCenterOfTread_4619323();
-void wheel18x14mmWithPinHole_6359828();
-
 
 
 
