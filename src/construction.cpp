50e3964658b323256e513f39db024a0459bf2cd7
NwaitDev <nathangallone@hotmail.com>
2023-05-04 15:01:35 +0200

    TOUT IRA BIEN

--- src/construction.cpp
+++ src/construction.cpp
@@ -18,31 +18,15 @@
 //********************************************************************************
 
 void construction(float angle){
-    /*
-    Brick brick1 = brick6330960();
-    Brick brick2 = brick4666999();
-    Brick brick3 = brick4163533();
-    Brick brick4 = brick6159763();
-    
-    brick1.connect(0,0,brick2,0);
-    brick2.connect(1,1,brick3,90);
-    brick3.connect(0,1,brick4,0);
-    */
-    /*
-    for(size_t i = 0; i< brick1.getConnectorList().size(); ++i){
-        brick1[i].showonscreen();
-    }
-    */
 
     //NOTATION brick<numeroEtape>_<indexEtape>
 
     Brick brick1_0 = brick4249021();
-    Brick brick1_1 = brick6321305();
-    Brick brick1_3 = brick6282140();
-    Brick brick1_4 = brick4211807();
+    Brick brick1_1 = brick6299413();
+    Brick brick1_2 = brick6299413();
 
-    brick1_0.connect(1,0,brick1_1,0,true);
-    brick1_0.connect(3,0,brick1_2,0,true);
+    brick1_0.connect(1,0,brick1_1,0);
+    brick1_0.connect(3,0,brick1_2,0);
     
     Brick brick2_0 = brick6055519();
 
